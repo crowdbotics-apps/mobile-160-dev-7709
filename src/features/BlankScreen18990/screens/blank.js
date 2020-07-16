686a8194862eb593a3cc9c600f84562d5d146bcb
Crowdbotics <team@crowdbotics.com>
2020-07-16 11:32:49 +0000

    Update components

--- src/features/BlankScreen18990/screens/blank.js
+++ src/features/BlankScreen18990/screens/blank.js
@@ -22,7 +22,7 @@ export class _Blank extends React.Component {
     }
   }
 
-  state = { Input_3: "" }
+  state = {}
 
   render = () => (
     <View
@@ -92,88 +92,26 @@ export class _Blank extends React.Component {
       >
         Press me!
       </Button>
-      <Text
-        style={{
-          marginLeft: 0,
-          marginRight: 0,
-          marginTop: 5,
-          marginBottom: 5,
-          paddingLeft: 5,
-          paddingRight: 5,
-          paddingTop: 5,
-          paddingBottom: 5,
-          overflow: "visible",
-          textAlign: "left",
-          verticalAlign: "baseline",
-          fontSize: 20,
-          color: "#000000",
-          backgroundColor: "#ffffff",
-          fontStyle: "normal",
-          fontWeight: "normal",
-          borderColor: "#000000",
-          borderStyle: "solid",
-          borderWidth: 0,
-          borderLeftWidth: 0,
-          borderRightWidth: 0,
-          borderTopWidth: 0,
-          borderBottomWidth: 0,
-          borderRadius: 0
+      <Image
+        resizeMode="cover"
+        source={{
+          uri:
+            "https://d3nfddh2sbo9hk.cloudfront.net/media/project_component_resources/download_o1mMiFu.jpeg"
         }}
-      >
-        Sample text content
-      </Text>
-      <Text
-        style={{
-          marginLeft: 0,
-          marginRight: 0,
-          marginTop: 5,
-          marginBottom: 5,
-          paddingLeft: 5,
-          paddingRight: 5,
-          paddingTop: 5,
-          paddingBottom: 5,
-          overflow: "visible",
-          textAlign: "left",
-          verticalAlign: "baseline",
-          fontSize: 20,
-          color: "#000000",
-          backgroundColor: "#ffffff",
-          fontStyle: "normal",
-          fontWeight: "normal",
-          borderColor: "#000000",
-          borderStyle: "solid",
-          borderWidth: 0,
-          borderLeftWidth: 0,
-          borderRightWidth: 0,
-          borderTopWidth: 0,
-          borderBottomWidth: 0,
-          borderRadius: 0
-        }}
-      >
-        Sample text content
-      </Text>
-      <Input
-        placeholder="Sample text input placeholder"
-        editable={true}
-        textStyle={{ fontSize: 20, color: "#000000" }}
         style={{
           width: "100%",
+          height: 200,
           marginLeft: 0,
           marginRight: 0,
-          marginTop: 5,
-          marginBottom: 5,
-          paddingLeft: 5,
-          paddingRight: 5,
-          paddingTop: 5,
-          paddingBottom: 5,
+          marginTop: 0,
+          marginBottom: 0,
+          paddingLeft: 0,
+          paddingRight: 0,
+          paddingTop: 0,
+          paddingBottom: 0,
           overflow: "visible",
           textAlign: "left",
           verticalAlign: "baseline",
-          fontSize: 20,
-          color: "#000000",
-          backgroundColor: "#ffffff",
-          fontStyle: "normal",
-          fontWeight: "normal",
           borderColor: "#000000",
           borderStyle: "solid",
           borderWidth: 0,
@@ -183,41 +121,7 @@ export class _Blank extends React.Component {
           borderBottomWidth: 0,
           borderRadius: 0
         }}
-        value={this.state.Input_3}
-        onChangeText={nextValue => this.setState({ Input_3: nextValue })}
       />
-      <Button
-        style={{
-          width: "100%",
-          marginLeft: 0,
-          marginRight: 0,
-          marginTop: 5,
-          marginBottom: 5,
-          paddingLeft: 5,
-          paddingRight: 5,
-          paddingTop: 5,
-          paddingBottom: 5,
-          overflow: "visible",
-          textAlign: "center",
-          verticalAlign: "baseline",
-          fontSize: 20,
-          color: "#000000",
-          backgroundColor: "#3366FF",
-          fontStyle: "normal",
-          fontWeight: "normal",
-          borderColor: "#000000",
-          borderStyle: "solid",
-          borderWidth: 0,
-          borderLeftWidth: 0,
-          borderRightWidth: 0,
-          borderTopWidth: 0,
-          borderBottomWidth: 0,
-          borderRadius: 0
-        }}
-        onPress={() => alert("Pressed!")}
-      >
-        Press me!
-      </Button>
     </View>
   )
 }
